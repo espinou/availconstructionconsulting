9ce3a6c5eab37c28285919f8a1a4441971dac96a
espinou <72425737+espinou@users.noreply.github.com>
2020-11-05 16:31:54 -0800

    Add files via upload
    initial commit

---
+++ avail-btn.js
@@ -0,0 +1,158 @@
+$(document).ready(() => {
+    // Button for email form
+    $('#btn1').on('mouseenter', () => {
+        $('#btn1').css({
+          color: 'rgb(0,0,0)',
+          backgroundColor: 'transparent',
+          border: '2px solid black' 
+        });
+    }); 
+    
+    $('#btn1').on('mouseleave', () => {
+      $('#btn1').css({
+        color: 'rgb(255,255,255)',
+        backgroundColor: '#FBB040' ,
+        border: 'none'
+      });
+  }); 
+  
+  $('#btn2').on('mouseenter', () => {
+    $('#btn2').css({
+      color: 'rgb(0,0,0)',
+      backgroundColor: 'transparent',
+      border: '2px solid black' 
+    });
+  }); 
+  
+  $('#btn2').on('mouseleave', () => {
+  $('#btn2').css({
+    color: 'rgb(255,255,255)',
+    backgroundColor: '#305A6B' ,
+    border: 'none'
+    });
+  }); 
+  
+  $('#btn3').on('mouseenter', () => {
+    $('#btn3').css({
+      color: 'rgb(0,0,0)',
+      backgroundColor: 'transparent',
+      border: '2px solid black' 
+    });
+  }); 
+  
+  $('#btn3').on('mouseleave', () => {
+  $('#btn3').css({
+    color: 'rgb(255,255,255)',
+    backgroundColor: '#305A6B' ,
+    border: 'none'
+    });
+  }); 
+  
+  $('#btn4').on('mouseenter', () => {
+    $('#btn4').css({
+      color: 'rgb(0,0,0)',
+      backgroundColor: 'transparent',
+      border: '2px solid black' 
+    });
+  }); 
+  
+  $('#btn4').on('mouseleave', () => {
+  $('#btn4').css({
+    color: 'rgb(255,255,255)',
+    backgroundColor: '#305A6B' ,
+    border: 'none'
+    });
+  }); 
+  
+  $('#yellow-btn').on('mouseenter', () => {
+    $('#yellow-btn').css({
+      color: 'rgb(0,0,0)',
+      backgroundColor: 'transparent',
+      border: '2px solid black' 
+    });
+  }); 
+  
+  $('#yellow-btn').on('mouseleave', () => {
+  $('#yellow-btn').css({
+    color: 'rgb(255,255,255)',
+    backgroundColor: '#FBB040' ,
+    border: 'none'
+    });
+  }); 
+  
+  $('#trans-btn').on('mouseenter', () => {
+    $('#trans-btn').css({
+      color: 'rgb(255,255,255)',
+      backgroundColor: 'transparent' ,
+      border: '2px solid white' 
+    });
+  }); 
+  
+  $('#trans-btn').on('mouseleave', () => {
+  $('#trans-btn').css({
+    color: 'white',
+    backgroundColor: '#FBB040' ,
+    border: 'none' 
+    });
+  }); 
+  
+  $('#trans-btn2').on('mouseenter', () => {
+    $('#trans-btn2').css({
+      color: 'rgb(0,0,0)',
+      backgroundColor: 'transparent',
+      border: '2px solid black' 
+    });
+  }); 
+  
+  $('#trans-btn2').on('mouseleave', () => {
+  $('#trans-btn2').css({
+    color: 'rgb(255,255,255)',
+    backgroundColor: '#FBB040',
+    border: 'none' 
+    });
+  }); 
+  
+  $('#trans-btn3').on('mouseenter', () => {
+    $('#trans-btn3').css({
+      color: 'rgb(255,255,255)',
+      backgroundColor: 'transparent',
+      border: '2px solid white' 
+    });
+  }); 
+  
+  $('#trans-btn3').on('mouseleave', () => {
+  $('#trans-btn3').css({
+    color: 'rgb(255,255,255)',
+    backgroundColor: '#FBB040',
+    border: 'none' 
+    });
+  }); 
+
+  $('.submit').on('mouseenter', () => {
+    $('.submit').css({
+      color: 'rgb(0,0,0)',
+      backgroundColor: 'transparent',
+      border: '2px solid rgb(0,0,0)' 
+    });
+  }); 
+  
+  $('.submit').on('mouseleave', () => {
+  $('.submit').css({
+    color: 'rgb(255,255,255)',
+    backgroundColor: '#FBB040' ,
+    border: 'none' 
+    });
+  }); 
+  
+  
+  
+  
+  
+  $('.sub-btn').on('mouseclick', () => {
+    $('.sub-btn').css({
+      color: '#EDDED1',
+      backgroundColor: 'rgba(46, 59, 79, 1)' 
+    });
+  }); 
+  
+  });
